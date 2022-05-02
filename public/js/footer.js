82558466462910d7cfde06d6b40a052fa0db80aa
bayramova-g <gulsumrb@code.edu.az>
2022-05-03 01:18:51 +0400

    done

---
+++ public/js/footer.js
@@ -0,0 +1,81 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+    <div class="footer-content">
+    <img src="/img/vanessaa.png" class="logo" alt="" />
+
+    <div class="footer-ul-container">
+      <ul class="category">
+        <li class="category-title">women</li>
+
+        <li><a href="#" class="footer-link">t-shirt</a></li>
+        <li><a href="#" class="footer-link">trousers</a></li>
+        <li><a href="#" class="footer-link">sweatshirt</a></li>
+        <li><a href="#" class="footer-link">jeans</a></li>
+        <li><a href="#" class="footer-link">shoes</a></li>
+        <li><a href="#" class="footer-link">casual</a></li>
+        <li><a href="#" class="footer-link">watch</a></li>
+        <li><a href="#" class="footer-link">sports</a></li>
+        <li><a href="#" class="footer-link">shirts</a></li>
+        <li><a href="#" class="footer-link">formals</a></li>
+      </ul>
+      <ul class="category">
+        <li class="category-title">men</li>
+
+        <li><a href="#" class="footer-link">t-shirt</a></li>
+        <li><a href="#" class="footer-link">trousers</a></li>
+        <li><a href="#" class="footer-link">sweatshirt</a></li>
+        <li><a href="#" class="footer-link">jeans</a></li>
+        <li><a href="#" class="footer-link">shoes</a></li>
+        <li><a href="#" class="footer-link">casual</a></li>
+        <li><a href="#" class="footer-link">watch</a></li>
+        <li><a href="#" class="footer-link">sports</a></li>
+        <li><a href="#" class="footer-link">shirts</a></li>
+        <li><a href="#" class="footer-link">formals</a></li>
+      </ul>
+    </div>
+  </div>
+  <p class="footer-title">about company</p>
+  <p class="info">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima maiores
+    rerum reiciendis incidunt. Dolorum suscipit velit impedit harum. Laborum
+    corrupti quaerat porro praesentium autem possimus fugit veritatis?
+    Aliquam alias minima illum error perspiciatis! Provident libero
+    architecto aut vitae facilis dolor aliquid atque ea similique deleniti
+    ullam, incidunt autem? Fuga, commodi veritatis, molestiae explicabo
+    corporis optio consectetur officia doloribus fugiat vero magni
+    voluptatibus nam iusto perferendis debitis reprehenderit? Molestiae,
+    aspernatur. Nulla enim vitae adipisci deleniti, laudantium veniam.
+    Ratione officiis error asperiores, amet minus ipsum eos laboriosam earum
+    similique molestias perspiciatis numquam enim minima. Repellat
+    accusantium officiis impedit totam velit, eligendi nostrum.
+  </p>
+
+  <p class="infor"> 
+  <i class="fa-solid fa-phone"></i>  telephone - 050 461 55 85</p>
+  <p class="infor">
+  <i class="fa-solid fa-envelope"></i>
+  e-mail : bayramevaa@gmail.com
+  </p>
+
+  <div class="footer-social-container">
+    <div>
+      <a href="#" class="social-link">terms & services</a>
+      <a href="#" class="social-link">privacy page</a>
+    </div>
+    <div>
+      <a href="#" class="social-link"><i class="fa-brands fa-instagram"></i>instagram</a>
+      <a href="#" class="social-link"> <i class="fa-brands fa-facebook-f"></i>facebook</a>
+      <a href="#" class="social-link"> <i class="fa-brands fa-twitter"></i> twitter</a>
+ 
+    </div>
+  </div>
+
+  <p class="footer-credit">Vanessa , Best apparels online store</p>
+`;
+
+
+}
+
+    createFooter();
